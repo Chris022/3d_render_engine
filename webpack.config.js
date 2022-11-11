a8c8b0e73df5374e42b831fced4d6de2a1f57d87
Christoph <christoph@debian>
2022-11-11 21:27:35 +0100

    :building_construction: add webpack and switch to typescript

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    devServer: {
+        static: './dist',
+    },
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                  {
+                    loader: 'raw-loader',
+                    options: {
+                      esModule: false,
+                    },
+                  },
+                ],
+              },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+    }
+};

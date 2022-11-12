f64ae762e665992bcae765ca15e7bb0734ff525e
Christoph <christoph@debian>
2022-11-12 19:25:15 +0100

    :bug: fix export bug

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,17 +1,5 @@
-import * as data_types from "./types";
-declare const _default: {
-    OutlineMaterial: (edge_color: string, fill_color: string, line_width: number) => data_types.Material;
-    OutlineMaterialGlow: (edge_color: string, fill_color: string, line_width: number, glow_stength: number) => data_types.Material;
-    move_shape3D: (shape: data_types.Shape3D, move_vec: data_types.Point3D) => data_types.Shape3D;
-    init_canvas: (options: data_types.Options) => void;
-    get_width: (options: data_types.Options) => number;
-    get_height: (options: data_types.Options) => number;
-    Point3D: (x: number, y: number, z: number) => data_types.Point3D;
-    Shape3D: (edges: data_types.Point3D[], material: data_types.Material) => data_types.Shape3D;
-    Scene3D: (shapes: data_types.Shape3D[]) => data_types.Shape3D[][];
-    Point2D: (x: number, y: number) => data_types.Point2D;
-    Shape2D: (edges: data_types.Point2D[], material: data_types.Material) => data_types.Shape2D;
-    Scene2D: (shapes: data_types.Shape2D[]) => data_types.Shape2D[][];
-    Options: (fix_size: number, canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) => data_types.Options;
-};
-export default _default;
+export * from "./types";
+export * from "./draw_helpers";
+export * from "./canvas_helpers";
+export * from "./shape_helpers";
+export * from "./material_helpers";
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,6 @@
-import * as data_types from "./types"
-import { draw_scene3D } from "./draw_helpers"
+export * from "./types"
+export * from "./draw_helpers"
 
-import * as canvas_functions from "./canvas_helpers";
-import * as shape_functions from "./shape_helpers";
-import * as materials from "./material_helpers";
-
-export default {
-    ...data_types,
-    ...draw_scene3D,
-    ...canvas_functions,
-    ...shape_functions,
-    ...materials
-};
+export * from "./canvas_helpers";
+export * from "./shape_helpers";
+export * from "./material_helpers";

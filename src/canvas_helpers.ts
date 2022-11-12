05538e6fc5be391f671d95e8287afd3222763df5
Christoph <christoph@debian>
2022-11-12 15:30:40 +0100

    :bug: fix wrong scaleing of canvas bug

--- src/canvas_helpers.ts
+++ src/canvas_helpers.ts
@@ -5,27 +5,15 @@ export let scale_canvas = (canvas: HTMLCanvasElement,context:CanvasRenderingCont
     context.setTransform(1, 0, 0, 1, 0, 0);
     context.translate(canvas.width / 2, canvas.height / 2);
 
-    if(canvas.width > canvas.height){
-        context.scale(canvas.height/fixed_size,canvas.height/fixed_size)
-    }else{
-        context.scale(canvas.width/fixed_size,canvas.width/fixed_size)
-    }
+    context.scale(canvas.height/fixed_size,canvas.height/fixed_size)
     
     context.scale(1,-1);
 }
 
 export let get_width = (canvas:HTMLCanvasElement,fixed_size: number):number => {
-    if(canvas.width > canvas.height){
-        return canvas.width*fixed_size/canvas.height
-    }else{
-        return fixed_size
-    }
+    return canvas.width*fixed_size/canvas.height
 }
 
 export let get_height = (canvas:HTMLCanvasElement,fixed_size: number):number => {
-    if(canvas.width > canvas.height){
-        return fixed_size
-    }else{
-        return canvas.height*fixed_size/canvas.width
-    }
+    return fixed_size
 }
